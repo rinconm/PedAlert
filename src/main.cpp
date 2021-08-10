0819579083c98606154d9537ccb8002eeb656eac
Marcos <marcosrincon@ymail.com>
2021-08-09 18:38:07 -0700

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+/*
+Pseudocode
+
+Initialize
+
+1. Function Call - Establish network connection
+
+while(TRUE){
+  2. Function Call - Detect Light
+  If light
+    restart while loop
+  Else
+    continue
+  
+  3. Function Call - Detect for pedestrians in waiting area
+  If pedestrians
+    continue
+  Else
+    restart while loop
+    
+  4. Function Call - Detect for pedestrians across walkpath
+  If pedestrians crossing
+    continue
+  Else
+    Return to 3. Function Call - Detect for pedestrians in waiting area
+    
+  5. Function Call - Detect for oncoming vehicles
+  If vehicles
+    continue
+  Else
+    Return to Function Call - Detect for pedestrians across walkpath
+    
+  6. Function Call - Measure velocity of oncoming car
+  If slowing down or at a halt
+    continue
+  Else
+    Function call - Alert protocol
+    
+  7. Function Call - Measure velocity of slowing or halted car
+  If vehicle speeds up
+    Return to Function Call - Detect for pedestrians across walkpath
+  Else
+    Return to 7. Function Call - Measure velocity of slowing or halted car
+
+}
+
+*/
+
