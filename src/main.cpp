2327f0675e0c0bd0e6720342e6983ec9fc14c880
Marcos <marcosrincon@ymail.com>
2021-09-01 22:07:10 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*
  * @file main.cpp
- * @brief The Global Entry Point
+ * @brief 
  * @author Marcos Rincon
  * @bug None
  */
@@ -11,10 +11,48 @@
 using namespace std;
 using namespace pedalert;
 
+int light, nightflag, vel;
+bool ped, pedwalk, veh;
+
 int main() {
-  
+  // Establish network connection function
+  network();
+  // Parent loop. Infinite main loop to ensure automatic operation.
   while(true) {
-    
+    // Detect light level function
+    lightdetect();
+    // If daytime, restart parent while loop
+    if (light > nightflag) {
+      continue
+    }
+    // First child while loop. To return to detect pedestrians in waiting area
+    while(true) {
+      // Detect for pedestrians in waiting area function
+      ped = waitdetect();
+      // If no pedestrian, restart parent while loop
+      if (ped == 0) {
+        break
+      }
+      // Second child while loop. To return to detect for movement across walk path
+      while(true) {
+        // Detect for movement across walk path function
+        walkdetect();
+        // If no pedestrian, restart first child while loop
+        if (pedwalk == 0) {
+          break
+        }
+        // Detect for oncoming vehicles
+        vehdetect();
+        // If no vehicle, restart second child while loop
+        if (veh == 0) {
+          continue
+        }
+        // stoped here
+        // Measure velocity of oncoming vehicles function
+        vel = velocity()
+        if (vel <= 
+      }
+    }
   }
     
   return 0; 
@@ -22,6 +60,30 @@ int main() {
 
 void network() {
   
+}
+
+void lightdetect() {
+  
+}
+
+void waitdetect() {
+  
+}
+
+void walkdetect() {
+  
+}
+
+void vehdetect() {
+  
+}
+
+void velocity() {
+  
+}
+
+void alert() {
+
 }
 
 /*
@@ -69,18 +131,6 @@ while(TRUE){
     Return to 7. Function Call - Measure velocity of slowing or halted car
 
 }
-
-
-
-// Functions
-1. Establish network connection
-2. Light detection
-3. Detect for pedestrians in waiting area
-4. Detect for pedestrians across walkpath
-5. Detect for oncoming vehicles
-6. Measure velocity of oncoming cars
-7. Alert protocol
-8. Measure velocity of slowing or halted cars
 
 */
 
