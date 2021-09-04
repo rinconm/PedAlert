5eb4680869e656a5e114266485e10a8a27e43732
Marcos <marcosrincon@ymail.com>
2021-09-04 08:59:22 -0700

    Update main.hpp

--- include/main.hpp
+++ include/main.hpp
@@ -1,119 +1,31 @@
-  const int PHOTORESISTOR_PIN = 25, RELAY_PIN1 = 5, RELAY_PIN2 = 6;
+/**
+ * @file main.hpp
+ * @brief Contains all Constants and Standard Functionality
+ * @author Marcos Rincon
+ * @bug 
+ */
 
-  // the setup function runs once when you press reset or power the board
-  void setup() {
-  // initialize digital pin as an output.
-  pinMode(RELAY_PIN, OUTPUT);
-  // Set baud rate to 9600
-  Serial.begin(9600);
-  // Begin radio transmission
-  radio.begin();
-  // 
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  Serial.printIn("Ready");
-  }
+#pragma once
 
-void network() {
-  // Transmitter code:
-
-  // Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-  #include <SPI.h>
-  #include <nRF24L01.h>
-  #include <RF24.h>
-  RF24 radio(7, 8); // CE, CSN
-  const byte address[6] = "00001";
-  void setup() 
-    radio.stopListening();
-  }
-  void loop() {
-    const char text[] = "Hello World";
-    radio.write(&text, sizeof(text));
-    delay(1000);
-  }
-
-  // Receiver code:
-  /*
-  * Arduino Wireless Communication Tutorial
-  *       Example 1 - Receiver Code
-  *                
-  * by Dejan Nedelkovski, www.HowToMechatronics.com
-  * 
-  * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-  */
-  #include <SPI.h>
-  #include <nRF24L01.h>
-  #include <RF24.h>
-  RF24 radio(7, 8); // CE, CSN
-  const byte address[6] = "00001";
-  void setup() {
-    radio.startListening();
-  }
-  void loop() {
-    if (radio.available()) {
-      char text[32] = "";
-      radio.read(&text, sizeof(text));
-      Serial.println(text);
-    }
-  }  
-}
-
-
-const int NIGHT_VALUE = 500;
-
-bool lightdetect() {
-  // Read Light Sensor Value
-  int value = analogRead(PHOTORESISTOR_PIN);
-  // Return a 0 for day-time and 1 for night-time
-  if value > NIGHT_VALUE
-    return 0
-  else
-    return 1
-}
-
-// Radar scan speed of objects in vicinity
-float radarscan() {
-float v = Serial.parseFloat();
-return v;
-}
-
-
-
+#include <string>
+#include <array>
 #include <vector>
-void waitdetect() {
-  
-  const int start = 0;
-            end = 1000;
-  for (i = start; num <= end; i++) {
-    v(i) = radarscan();
-    delay(1)
-    
-  }
-  
-  
-}
 
-void walkdetect() {
-  
-}
+// Pin Assignment
+#define PHOTORESISTOR_PIN 25 
+#define RELAY_PIN1 5 
+#define RELAY_PIN2 6
+#define NETWORK_CE = 7
+#define NETWORK_CSN = 8
+// Photoresistor Analog Night Value
+#define NIGHT_VALUE 500
+// Address of Network Communication Channel
+#define BYTE_ADDRESS 00001
+// Radar Scan Interval (Used in radarscan() for loop)
+#define START_SCAN = 0;
+#define END_SCAN = 1000;
+// Minimum Walking Speed of Pedestrian Value
+#define MIN_WALK_SPEED 2
+// Minimum Car Speed Value
+#define MIN_CAR_SPEED 15
 
-void vehdetect() {
-  
-}
-
-void velocityapproach() {
-  
-}
-
-void alert() {
-  // Turn on DC and AC components connected to the relays for 5 seconds then shut off
-  digitalWrite(RELAY_PIN1, HIGH);
-  digitalWrite(RELAY_PIN2, HIGH);
-  delay(5000);
-  digitalWrite(RELAY_PIN1, LOW);
-  digitalWrite(RELAY_PIN2, LOW);
-}
-            
-void velocityexit(){
-  
-}
